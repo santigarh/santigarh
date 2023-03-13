a106623d909072faea94290d9f3a5201b389592c
Santiago <santigarh.77@gmail.com>
2023-03-13 11:24:16 -0500

    Config inicial

---
+++ src/components/icons/index.js
@@ -0,0 +1,37 @@
+import DarkLightIcon from "./DarkLightIcon.astro";
+import IconSun from "./IconSun.astro";
+import IconMoon from "./IconMoon.astro";
+import IconGithub from "./IconGithub.astro";
+import IconTwitter from "./IconTwitter.astro";
+import IconArrowRight from "./IconArrowRight.astro";
+import IconMenu from "./IconMenu.astro";
+import IconArrowDownRight from "./IconArrowDownRight.astro";
+import IconDK from "./IconDK.astro";
+import IconLinkedIn from "./IconLinkedIn.astro";
+import IconMail from "./IconMail.astro";
+import IconJS from "./IconJS.astro";
+import IconTailwind from "./IconTailwind.astro";
+import IconArduino from "./IconArduino.astro";
+import IconPHP from "./IconPHP.astro";
+import IconAstro from "./IconAstro.astro";
+import IconAstroDark from "./IconAstroDark.astro";
+
+export {
+  DarkLightIcon,
+  IconSun,
+  IconMoon,
+  IconGithub,
+  IconTwitter,
+  IconArrowRight,
+  IconMenu,
+  IconArrowDownRight,
+  IconDK,
+  IconLinkedIn,
+  IconMail,
+  IconJS,
+  IconTailwind,
+  IconArduino,
+  IconPHP,
+  IconAstro,
+  IconAstroDark,
+};
